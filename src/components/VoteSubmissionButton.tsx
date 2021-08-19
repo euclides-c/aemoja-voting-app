1ca30b823743561225aaf7f4f1bb1486b993cd62
euclides-c <euclideschauque@gmail.com>
2021-08-20 00:46:08 +0900

    added submission button

--- src/components/VoteSubmissionButton.tsx
+++ src/components/VoteSubmissionButton.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import { Form, Input, Button } from 'antd';
 
-const VoteSubmissionButton = () => {
-	return <div></div>;
+const VoteSubmissionButoon = () => {
+	const onFinish = (values: any) => {
+		console.log('Success:', values);
+	};
+
+	const onFinishFailed = (errorInfo: any) => {
+		console.log('Failed:', errorInfo);
+	};
+
+	return (
+		<Form
+			name='basic'
+			labelCol={{
+				span: 8,
+			}}
+			wrapperCol={{
+				span: 8,
+			}}
+			initialValues={{
+				remember: true,
+			}}
+			onFinish={onFinish}
+			onFinishFailed={onFinishFailed}>
+			<Form.Item
+				label='Vote Token'
+				name='username'
+				rules={[
+					{
+						required: true,
+						message: 'Please Input Your Voting Token',
+					},
+				]}>
+				<Input />
+			</Form.Item>
+
+			<Form.Item
+				wrapperCol={{
+					offset: 8,
+					span: 16,
+				}}>
+				<Button type='primary' htmlType='submit'>
+					Submit
+				</Button>
+			</Form.Item>
+		</Form>
+	);
 };
 
-export default VoteSubmissionButton;
+export default VoteSubmissionButoon;
