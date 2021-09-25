d722d5710462290f8dabd352223d7d3008218f90
euclides-c <euclideschauque@gmail.com>
2021-09-25 12:02:11 +0900

    Revert "Revert "working server""
    This reverts commit 43a9f2f51aae892ee40cec02701c1227d00558db.

---
+++ server/common/services/mongoose.service.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import debug, { IDebugger } from 'debug';
+
+const log: IDebugger = debug('app:mongoose-service');
+
+class MongooseService {
+	private count = 0;
+	private mongooseOptions = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		serverSelectionTimeoutMS: 5000,
+	};
+
+	constructor() {
+		this.connectWithRetry();
+	}
+
+	getMongoose() {
+		return mongoose;
+	}
+
+	connectWithRetry = () => {
+		log('Attempting MongoDB connection (will retry if needed)');
+		mongoose
+			.connect(
+				'mongodb+srv://***REMOVED***db:***REMOVED***@cluster0.6tv0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+				this.mongooseOptions
+			)
+			.then(() => {
+				log('MongoDB is connected');
+			})
+			.catch((err) => {
+				const retrySeconds = 5;
+				log(
+					`MongoDB connection unsuccessful (will retry #${++this
+						.count} after ${retrySeconds} seconds):`,
+					err
+				);
+				setTimeout(this.connectWithRetry, retrySeconds * 1000);
+			});
+	};
+}
+export default new MongooseService();
