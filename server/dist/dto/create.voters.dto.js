a33e89db45d54e50cfc52a2d3f631c6bacdbae52
euclides-c <euclideschauque@gmail.com>
2021-09-25 11:44:37 +0900

    removing dist directory from server

--- server/dist/dto/create.voters.dto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Link for the photo, do I Upload the photo the amazon s3, get and get the link?
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JlYXRlLnZvdGVycy5kdG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9kdG8vY3JlYXRlLnZvdGVycy5kdG8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFZQSxpRkFBaUYifQ==
