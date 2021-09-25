205b9fab4bfc84585e66ab59991058e06134ff31
euclides-c <euclideschauque@gmail.com>
2021-09-26 07:42:07 +0900

    refactoring emailnotification logic unto its own service file
    Former-commit-id: b5d75d2aea3b4ef56b706b1c96b5a9d9d0205da4 [formerly bef9fd4a79c8a1ea43022a4eaae86b5b55c18160]
    Former-commit-id: bdbe68a978ec057a60aa68ed9e3f0ab1d1c77917

---
+++ server/common/services/emailnotification.service.ts
@@ -0,0 +1,55 @@
+import nodemailer from 'nodemailer';
+
+const Emailnotification = async (
+	receiver: string,
+	candidateName: string,
+	token: string,
+	action: string
+) => {
+	if (action != 'registration' && action != 'vote') {
+		throw new Error("Action is invalid, please pass 'registration' or 'vote'");
+	}
+
+	const REGISTRATION_SUBJECT =
+		'Confirmação de Registo na Plataforma Eleitoral da  AEMOJA';
+	const REGISTRATION_MESSAGE = `Acabou de registar-se na Plataforma Eleitoral da AEMOJA. Quando for votar, use o código seguinte para validar o seu voto: ${token}`;
+	const VOTE_CONFIRMATION_SUBJECT =
+		'O Seu Voto Foi Registado Na Plataforma Eleitoral da AEMOJA';
+	const VOTE_CONFIRMATION_MESSAGE = `Votou em ${candidateName} na eleição presidencial da AEMOJA"`;
+
+	// Transport object definition
+
+	const transporter = nodemailer.createTransport({
+		host: '***REMOVED***',
+		port: 465,
+		secure: true,
+		auth: {
+			user: 'apikey',
+			pass: '***REMOVED***',
+		},
+	});
+
+	// send mail with defined transport object
+	if (action === 'registration') {
+		const info = await transporter.sendMail({
+			from: '"Plataforma Eleitoral da AEMOJA" <plataformaeleitoral@***REMOVED***.org>',
+			to: receiver,
+			subject: REGISTRATION_SUBJECT,
+			text: REGISTRATION_MESSAGE,
+			html: `<b> ${REGISTRATION_MESSAGE} </b>`,
+		});
+		return info.messageId;
+	}
+	if (action === 'vote') {
+		const info = await transporter.sendMail({
+			from: '"Plataforma Eleitoral da AEMOJA" <plataformaeleitoral@***REMOVED***.org>',
+			to: receiver,
+			subject: VOTE_CONFIRMATION_SUBJECT,
+			text: VOTE_CONFIRMATION_MESSAGE,
+			html: `<b> ${VOTE_CONFIRMATION_MESSAGE} </b>`,
+		});
+		return info.messageId;
+	}
+};
+
+export default Emailnotification;
--- server/controllers/vote.controller.ts
+++ server/controllers/vote.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import debug, { IDebugger } from 'debug';
 import voteService from '../services/vote.service';
+import Emailnotification from '../common/services/emailnotification.service';
 
 const log: IDebugger = debug('app:vote-controller');
 
@@ -9,10 +10,17 @@ class VoteController {
 		// takes email, token, and candidate voted
 		// check email and and token validity, write a middleware
 		const vote = await voteService.castVote(req.body);
-		voteService.VoteConfirmationEmail(
+		Emailnotification(
 			req.body.votersEmail,
-			req.body.candidateName
+			req.body.candidateName,
+			' ',
+			'vote'
 		);
+
+		// voteService.VoteConfirmationEmail(
+		// 	req.body.votersEmail,
+		// 	req.body.candidateName
+		// );
 
 		res.status(200).send(vote);
 	}
