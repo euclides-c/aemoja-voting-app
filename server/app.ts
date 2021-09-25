a4969ccd50f12dbfd871ba091bbed549425fa2d8
euclides-c <euclideschauque@gmail.com>
2021-09-25 12:02:11 +0900

    Revert "Revert "working server""
    This reverts commit 3dfba26ec3141c0fae0d009e5a7cadf16b326bf6 [formerly 70883031ba05013b0a51c2b816719304a71ccbd3] [formerly d2b53df9e2f619cd7f494208a523630af18d84ec [formerly 43a9f2f51aae892ee40cec02701c1227d00558db]].
    
    
    Former-commit-id: 85ee1eb94945854a4c3749cc11290ce74d1d8385 [formerly d722d5710462290f8dabd352223d7d3008218f90]
    Former-commit-id: bce606e614f0f3895f8c6c6e5a1f5296921ed426

---
+++ server/app.ts
@@ -0,0 +1,59 @@
+import express, { Application, json, Request, Response } from 'express';
+import { createServer, Server } from 'http';
+import { format, transports } from 'winston';
+import { LoggerOptions, logger } from 'express-winston';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import debug, { IDebugger } from 'debug';
+
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { VotersRoutes } from './routes/voters.routes.config';
+import { VoteRoutes } from './routes/vote.routes.config';
+import { ResultRoutes } from './routes/result.routes.config';
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+	throw dotenvResult.error;
+}
+const app: Application = express();
+const server: Server = createServer(app);
+const port = 5000;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: IDebugger = debug('app');
+
+const corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(json());
+app.use(cors(corsOptions));
+
+const loggerOptions: LoggerOptions = {
+	transports: [new transports.Console()],
+	format: format.combine(
+		format.json(),
+		format.prettyPrint(),
+		format.colorize({ all: true })
+	),
+};
+
+if (!process.env.DEBUG) {
+	loggerOptions.meta = true; // when not debugging, make terse
+}
+
+app.use(logger(loggerOptions));
+
+routes.push(new VotersRoutes(app));
+routes.push(new VoteRoutes(app));
+routes.push(new ResultRoutes(app));
+
+const runningMessage = `Server running at http://localhost:${port}`;
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send(runningMessage);
+});
+server.listen(port, () => {
+	routes.forEach((route: CommonRoutesConfig) => {
+		debugLog(`Routes configured for ${route.getName()}`);
+	});
+	console.log(runningMessage);
+});
