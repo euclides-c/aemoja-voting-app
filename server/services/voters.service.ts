9a948ab8d7de61fa5dce543fed0ebbbeca79bd45
euclides-c <euclideschauque@gmail.com>
2021-09-26 07:48:09 +0900

    refactoring getS3link logic unto its own service file

--- server/services/voters.service.ts
+++ server/services/voters.service.ts
@@ -68,32 +68,32 @@ class VotersService {
 	// 	return info.messageId;
 	// }
 
-	async getS3link() {
-		const region = 'ap-northeast-1';
-		const bucketName = '***REMOVED***';
-		const accessKeyId = '***REMOVED***';
-		const secretAccessKey = '***REMOVED***';
+	// async getS3link() {
+	// 	const region = 'ap-northeast-1';
+	// 	const bucketName = '***REMOVED***';
+	// 	const accessKeyId = '***REMOVED***';
+	// 	const secretAccessKey = '***REMOVED***';
 
-		const s3 = new aws.S3({
-			region,
-			accessKeyId,
-			secretAccessKey,
-			signatureVersion: 'v4',
-		});
+	// 	const s3 = new aws.S3({
+	// 		region,
+	// 		accessKeyId,
+	// 		secretAccessKey,
+	// 		signatureVersion: 'v4',
+	// 	});
 
-		const randomBytes = promisify(crypto.randomBytes);
-		const rawBytes = await randomBytes(16);
-		const imageName = rawBytes.toString('hex');
+	// 	const randomBytes = promisify(crypto.randomBytes);
+	// 	const rawBytes = await randomBytes(16);
+	// 	const imageName = rawBytes.toString('hex');
 
-		const params = {
-			Bucket: bucketName,
-			Key: imageName,
-			Expires: 3600,
-		};
+	// 	const params = {
+	// 		Bucket: bucketName,
+	// 		Key: imageName,
+	// 		Expires: 3600,
+	// 	};
 
-		const uploadURL = await s3.getSignedUrlPromise('putObject', params);
-		console.log(uploadURL);
-		return uploadURL;
-	}
+	// 	const uploadURL = await s3.getSignedUrlPromise('putObject', params);
+	// 	console.log(uploadURL);
+	// 	return uploadURL;
+	// }
 }
 export default new VotersService();
