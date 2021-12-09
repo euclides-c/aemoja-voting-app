1f28303ba05cb975e5486217fb2390be663c729f
euclides-c <euclideschauque@gmail.com>
2021-12-10 01:27:24 +0900

    rewrite
    Former-commit-id: b94a1605de6fe0cf48b1f85ddde1960196b99492 [formerly 17f16c47898bdcb482190549ee6f2cde28d2d9c9]
    Former-commit-id: 0516c60f00f003f2915d00b73517cb9eef379dbb

--- server/services/vote.service.ts
+++ server/services/vote.service.ts
@@ -18,31 +18,6 @@ class VoteService {
 		return VoteDao.getVoterByToken(token);
 	}
 
-	// async VoteConfirmationEmail(receiver: string, candidateName: string) {
-	// 	// let testAccount = await nodemailer.createTestAccount();
-
-	// 	let transporter = nodemailer.createTransport({
-	// 		host: '***REMOVED***',
-	// 		port: 465,
-	// 		secure: true, // true for 465, false for other ports
-	// 		auth: {
-	// 			user: 'apikey', // generated ethereal user
-	// 			pass: '***REMOVED***', // generated ethereal password
-	// 		},
-	// 	});
-
-	// 	// send mail with defined transport object
-	// 	let info = await transporter.sendMail({
-	// 		from: '"Plataforma Eleitoral da AEMOJA" <plataformaeleitoral@***REMOVED***.org>', // sender address
-	// 		to: receiver,
-	// 		subject: 'O Seu Voto Foi Registado Na Plataforma Eleitoral da AEMOJA', // Subject line
-	// 		text: `Votou em ${candidateName} na eleição presidencial da AEMOJA`,
-	// 		html: `<b>  votou ${candidateName} na eleição presidencial da AEMOJA </b>`, // html body
-	// 	});
-
-	// 	console.log('Message sent: %s', info.messageId);
-	// 	return info.messageId;
-	// }
 }
 
 export default new VoteService();
--- server/services/voters.service.ts
+++ server/services/voters.service.ts
@@ -42,58 +42,6 @@ class VotersService {
 		return VotersDao.getVoterByToken(token);
 	}
 
-	// async sendEmail(receiver: string, token: string) {
-	// 	// let testAccount = await nodemailer.createTestAccount();
 
-	// 	let transporter = nodemailer.createTransport({
-	// 		host: '***REMOVED***',
-	// 		port: 465,
-	// 		secure: true, // true for 465, false for other ports
-	// 		auth: {
-	// 			user: 'apikey',
-	// 			pass: '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',
-	// 		},
-	// 	});
-
-	// 	// send mail with defined transport object
-	// 	let info = await transporter.sendMail({
-	// 		from: '"Plataforma Eleitoral da AEMOJA" <plataformaeleitoral@***REMOVED***.org>', // sender address
-	// 		to: [receiver, 'deliodownload@gmail.com'], // list of receivers
-	// 		subject: 'Confirmação de Registo na Plataforma Eleitoral da  AEMOJA', // Subject line
-	// 		text: `Acabou de registar-se na Plataforma Eleitoral da AEMOJA. Quando for votar, use o código seguinte para validar o seu voto: ${token}`,
-	// 		html: `<b> Acabou de registar-se na Plataforma Eleitoral da AEMOJA. Quando for votar, use o código seguinte para validar o seu voto ${token}</b>`, // html body
-	// 	});
-
-	// 	console.log('Message sent: %s', info.messageId);
-	// 	return info.messageId;
-	// }
-
-	// async getS3link() {
-	// 	const region = 'ap-northeast-1';
-	// 	const bucketName = '***REMOVED***';
-	// 	const accessKeyId = '***REMOVED***';
-	// 	const secretAccessKey = '***REMOVED***';
-
-	// 	const s3 = new aws.S3({
-	// 		region,
-	// 		accessKeyId,
-	// 		secretAccessKey,
-	// 		signatureVersion: 'v4',
-	// 	});
-
-	// 	const randomBytes = promisify(crypto.randomBytes);
-	// 	const rawBytes = await randomBytes(16);
-	// 	const imageName = rawBytes.toString('hex');
-
-	// 	const params = {
-	// 		Bucket: bucketName,
-	// 		Key: imageName,
-	// 		Expires: 3600,
-	// 	};
-
-	// 	const uploadURL = await s3.getSignedUrlPromise('putObject', params);
-	// 	console.log(uploadURL);
-	// 	return uploadURL;
-	// }
 }
 export default new VotersService();
